aadc86620de8686183f83addb1673d0ffff88f20
Riyazrmp <63150193+Riyazrmp@users.noreply.github.com>
2024-05-01 01:19:49 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,15 +1,13 @@
-#include <iostream>
-#include <ncurses.h>
-#include <sstream>
-#include <ctime>
-#include <cstdlib>
-#include <fstream>
+#include <iostream>// Input/output stream
+#include <ncurses.h>// Library for creating text-based user interfaces
+#include <sstream>// String stream operations
+#include <ctime>// Date and time functions
+#include <cstdlib>  // Standard library functions
+#include <fstream> // File input/output operations
 
-#include "src/Menu.h"
-
+#include "src/Menu.h" // User-defined header file for Menu class
 int main() {
-  Menu* menu = new Menu();
-  menu->showAndChooseMenuOptions();
-  
+  Menu* menu = new Menu();  // Create a new Menu object using dynamic memory allocation
+  menu->showAndChooseMenuOptions(); // Call the showAndChooseMenuOptions() method of the Menu object
   return 0;
 }
