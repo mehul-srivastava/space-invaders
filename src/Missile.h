d91157a1e0a0fff0bb191c07676b76f919ca0fd9
Riyazrmp <63150193+Riyazrmp@users.noreply.github.com>
2024-05-01 01:32:02 +0800

    Update Missile.h

--- src/Missile.h
+++ src/Missile.h
@@ -1,18 +1,18 @@
 #ifndef missile_h
 #define missile_h
 
-#include "Sprite.h"
+#include "Sprite.h"// Include the header file for the base Sprite class
 
 class Missile: public Sprite {
 private:
-    int power;
-    bool friendly;
+    int power; // Integer representing the power of the missile
+    bool friendly; // Boolean flag indicating whether the missile is friendly or not
 
 public:
-    Missile(bool alignment);
-    Missile(bool alignment, int x, int y);
-    ~Missile();
-    void update() override;
+    Missile(bool alignment);// Constructor for the Missile class with alignment parameter
+    Missile(bool alignment, int x, int y);// Constructor for the Missile class with alignment and position parameters
+    ~Missile();// Destructor for the Missile class
+    void update() override;// Override the update function from the base Sprite class
 };
 
 #endif
